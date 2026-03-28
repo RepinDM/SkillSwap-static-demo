bb3cdf064cb41e8adfeec3511901570aad28b0e3
DMRepin <dmrepin26@yandex.ru>
2026-03-28 18:28:19 +0300

    feat: добавлены типы навыков и цвета категорий

--- src/entities/skill/types.ts
+++ src/entities/skill/types.ts
@@ -1,21 +1,15 @@
+import type { TSubcategory } from "@/entities/category/types";
 
-// Основной тип навыка (для списков, карточек)
-export type TSkill = {
+// Пользовательский навык, который он заполняет сам в форме создания навыка
+export type TUserSkill = {
   id: string // Уникальный идентификатор навыка
   userId: string // ID владельца навыка
-  title: string // Название навыка
+  subCategory: TSubcategory
+  title: string // Название пользовательского навыка
   description: string // Краткое описание навыка (используется в карточке и превью)
-  category: string // Основная категория
   type: TSkillType // Тип навыка: teach — пользователь учит, learn — пользователь хочет научиться
   image?: string // Главное изображение навыка (используется в карточке)
   createdDate: Date // Дата создания навыка. В приложении хранится как Date и используется для сортировки.
-}
-
-// Расширенный тип (для страницы детали)
-export type TSkillDetails = TSkill & {
-  images?: string[] // Дополнительные изображения навыка (галерея на странице)
-  subcategoryId?: string // ID подкатегории
-  updatedDate?: string // Дата последнего изменения навыка
 }
 
 // Тип навыка:
---
+++ src/shared/lib/Colors/categoryColors.ts
@@ -0,0 +1,15 @@
+// Цвет тега определяется не напрямую, а через CATEGORY_COLORS.
+//   Мы не используем hex в компонентах — только дизайн-токены.
+// Тут цвета фонов тега,в
+
+export const CATEGORY_COLORS = {
+  businessCareer: "var(--tag-business-career)",
+  creativityArt: "var(--tag-creativity-art)",
+  foreignLanguages: "var(--tag-foreign-languages)",
+  educationDevelopment: "var(--tag-education-development)",
+  homeComfort: "var(--tag-home-comfort)",
+  healthLifestyle: "var(--tag-health-lifestyle)",
+  plus: "var(--tag-plus)",
+} as const;
+
+export type CategoryColorKey = keyof typeof CATEGORY_COLORS;
