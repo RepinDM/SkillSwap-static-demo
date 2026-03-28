a1b2c373ea13f6b88ac0c106aae238a9fd6b64f2
Yaroslav Spichihin <yaspichihin@gmail.com>
2026-03-28 23:10:28 +0300

    add ui button

---
+++ src/shared/ui/Button.tsx
@@ -0,0 +1,34 @@
+import { type FC, type MouseEventHandler, type ReactNode } from "react";
+import styles from "./Button.module.scss";
+import clsx from "clsx";
+
+interface ButtonProps {
+  children: ReactNode;
+  onClick?: MouseEventHandler<HTMLButtonElement>;
+  variant?: "primary" | "secondary" | "tertiary";
+  disabled?: boolean;
+  iconLeft?: ReactNode;
+  iconRight?: ReactNode;
+}
+
+export const Button: FC<ButtonProps> = ({
+  children,
+  onClick,
+  variant = "primary",
+  disabled = false,
+  iconLeft,
+  iconRight,
+}) => {
+  return (
+    <button
+      type="button"
+      className={clsx(styles.button, styles[variant])}
+      onClick={onClick}
+      disabled={disabled}
+    >
+      {iconLeft && <span className={styles.iconLeft}>{iconLeft}</span>}
+      {children}
+      {iconRight && <span className={styles.iconRight}>{iconRight}</span>}
+    </button>
+  );
+};
