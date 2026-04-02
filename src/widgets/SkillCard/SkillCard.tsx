31ae67eb941b9d70d1360a1266cfc83816bdf255
Nastya Dubrovina <anadubro@yandex.ru>
2026-04-02 19:52:11 +0300

    feature:styles-cards

--- src/widgets/SkillCard/SkillCard.tsx
+++ src/widgets/SkillCard/SkillCard.tsx
@@ -3,6 +3,10 @@ import type { TSkillCard } from "@/entities/skill/types";
 import { Tag } from "@/shared/ui/Tag/Tag";
 import { User } from "@/shared/ui/User/User";
 
+import styles from "./SkillCard.module.scss";
+import { Button } from "@/shared/ui/Button/Button";
+import buttonLike from "@/shared/image/icons/like.svg";
+
 type Props = {
   card: TSkillCard;
 };
@@ -10,17 +14,23 @@ type Props = {
 export const SkillCard = ({ card }: Props) => {
   return (
     <>
-       <div key={card.teachSkill.id}>
-          <User 
-            name={card.user.name}
-            avatar={card.user.avatar}
-            city={card.user.city.name}
-            age={getAge(card.user.birthDate)}
-          />
+       <li className={styles.card} key={card.teachSkill.id}>
+          <div className={styles.user}>
+            <div className={styles.userInfo}>
+              <User
+                name={card.user.name}
+                avatar={card.user.avatar}
+                city={card.user.city.name}
+                age={getAge(card.user.birthDate)}
+                avatarSize={100}
+              />
+            </div>
+            <img src={buttonLike} alt="Кнопка добавления в избранное" />
+          </div>
 
           {/* 2. Чему пользователь УЧИТ (teachSkill) */}
-          <div>
-            <p>Может научить:</p>
+          <div className={styles.section}>
+            <h4 className={styles.title}>Может научить:</h4>
             <Tag
               label={card.teachSkill.title}
               bgColor={getCategoryColor(
@@ -30,10 +40,11 @@ export const SkillCard = ({ card }: Props) => {
           </div>
 
           {/* 3. Чему пользователь хочет научиться (learnSkills - это массив) */}
-          <div>
-            <p>Хочет научиться:</p>
-            <ul>
-              {card.learnSkills.map(skill => (
+          <div className={styles.section}>
+            <h4 className={styles.title}>Хочет научиться:</h4>
+
+            <ul className={styles.tags}>
+              {card.learnSkills.slice(0, 2).map(skill => (
                 <li key={skill.id}>
                   <Tag
                     label={skill.subcategory.name}
@@ -43,9 +54,22 @@ export const SkillCard = ({ card }: Props) => {
                   />
                 </li>
               ))}
+
+              {card.learnSkills.length > 2 && (
+                <li className={styles.more}>
+                  +{card.learnSkills.length - 2}
+                </li>
+              )}
             </ul>
           </div>
-        </div>
+
+          <Button
+            variant="primary"
+            onClick={() => console.log("click", card)}
+          >
+            Подробнее
+          </Button>
+        </li>
     </>
   );
 };
