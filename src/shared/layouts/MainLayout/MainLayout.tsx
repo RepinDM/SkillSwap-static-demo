31bc3c79399cafdb7a41280fea6fde609770c0e8
Polina Belozertseva <belozertsevapolina@yandex.ru>
2026-04-04 00:22:07 +0500

    feat: добавлен mainLayout

---
+++ src/shared/layouts/MainLayout/MainLayout.tsx
@@ -0,0 +1,20 @@
+// Добавлен Layout для отображения Header/Footer для разного контента
+
+import { Header } from "@/widgets/header/Header";
+import { Footer } from "@/widgets/Footer/Footer";
+import styles from "./MainLayout.module.scss";
+import React from "react";
+
+export interface LayoutProps {
+  children: React.ReactNode;
+}
+
+export const Layout = ({ children }: LayoutProps) => {
+  return (
+    <div className={styles.layout}>
+      <Header />
+      <main className={styles.main}>{children}</main>
+      <Footer />
+    </div>
+  );
+};
