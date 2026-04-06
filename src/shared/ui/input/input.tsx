5a8818761de2a986536f4693766e457b3f16d49a
Polina Belozertseva <belozertsevapolina@yandex.ru>
2026-04-06 21:48:00 +0500

    feat: добавлен ref в input

--- src/shared/ui/input/input.tsx
+++ src/shared/ui/input/input.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { forwardRef } from "react";
 import type { InputHTMLAttributes } from "react";
 import styles from "./input.module.scss";
 import type { InputType } from "./input.types";
@@ -11,37 +12,43 @@ interface InputProps extends Omit<
   label?: string;
   error?: string;
   hint?: string;
-  value: string;
+  value?: string;
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
   placeholder?: string;
   disabled?: boolean;
 }
 
-export const Input = ({
-  type = "text",
-  label,
-  error,
-  hint,
-  value,
-  onChange,
-  placeholder,
-  disabled = false,
-  ...props
-}: InputProps) => {
-  return (
-    <div className={styles.container}>
-      {label && <label className={styles.label}>{label}</label>}
-      <input
-        type={type}
-        className={`${styles.input} ${error ? styles.inputError : ""}`}
-        value={value}
-        onChange={onChange}
-        placeholder={placeholder}
-        disabled={disabled}
-        {...props}
-      />
-      {error && <span className={styles.errorText}>{error}</span>}
-      {!error && hint && <span className={styles.hintText}>{hint}</span>}
-    </div>
-  );
-};
+export const Input = forwardRef<HTMLInputElement, InputProps>(
+  (
+    {
+      type = "text",
+      label,
+      error,
+      hint,
+      value,
+      onChange,
+      placeholder,
+      disabled = false,
+      ...props
+    },
+    ref
+  ) => {
+    return (
+      <div className={styles.container}>
+        {label && <label className={styles.label}>{label}</label>}
+        <input
+          ref={ref}
+          type={type}
+          className={`${styles.input} ${error ? styles.inputError : ""}`}
+          value={value}
+          onChange={onChange}
+          placeholder={placeholder}
+          disabled={disabled}
+          {...props}
+        />
+        {error && <span className={styles.errorText}>{error}</span>}
+        {!error && hint && <span className={styles.hintText}>{hint}</span>}
+      </div>
+    );
+  }
+);
