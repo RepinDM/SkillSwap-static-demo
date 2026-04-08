d01203c9d2a3706cd60777898d07c4a07f4bea8d
Nastya Dubrovina <anadubro@yandex.ru>
2026-04-08 18:10:18 +0300

    fix:auth-login

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,2 +1,9 @@
-const LoginPage = () => <h1>LoginPage</h1>;
+import { LoginForm } from "@/features/auth/login/LoginForm";
+
+const LoginPage = () => (
+  <>
+    <h1>LoginPage</h1>
+    <LoginForm />
+  </>
+);
 export default LoginPage;
