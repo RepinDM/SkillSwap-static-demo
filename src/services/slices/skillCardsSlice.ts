93de998e8779c86115d8e47f371992d853a4170f
Nastya Dubrovina <anadubro@yandex.ru>
2026-04-09 17:52:16 +0300

    refactor:skill-page

--- src/services/slices/skillCardsSlice.ts
+++ src/services/slices/skillCardsSlice.ts
@@ -3,8 +3,6 @@ import type { TCategoryItem } from "@/entities/category/types";
 import { createSlice } from "@reduxjs/toolkit";
 import { fetchSkillCards } from "../actions/skills";
 
-// import { mockSkillCards } from "../mockSkills";
-
 
 interface SkillCardsState {
   allSkillCards: TSkillCard[];
